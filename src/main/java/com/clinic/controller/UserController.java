0213497a3e25f6f736b30af7117609694f7fe28a
Pavel Dorozhko <pavel.dorozhko@gmail.com>
2016-12-27 17:49:03 +0300

    mistake dependency

--- src/main/java/com/clinic/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.clinic.controller;
-
-public class UserController {
-}
---
+++ src/main/java/com/clinic/service/LoginManagedBean.java
@@ -0,0 +1,10 @@
+package com.clinic.service;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "loginBean")
+@SessionScoped
+public class LoginManagedBean {
+
+}
