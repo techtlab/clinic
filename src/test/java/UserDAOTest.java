64d932af9b5fc3f65782b24a05f69e953b36addb
Pavel Dorozhko <pavel.dorozhko@gmail.com>
2016-12-28 16:17:08 +0300

    downgrade junit test

--- src/test/java/UserDAOTest.java
+++ src/test/java/UserDAOTest.java
@@ -1,26 +1,24 @@
 import com.clinic.dao.impl.UserDAOImpl;
 import com.clinic.model.User;
-import org.junit.gen5.api.Assertions;
-import org.junit.gen5.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UserDAOTest {
-
-
 
     @Test
     public void testGetUserById() {
         User user = UserDAOImpl.getUser(1);
-        Assertions.assertNotNull(user);
-        Assertions.assertEquals(user.getLogin(), "Mark");
-        Assertions.assertEquals(user.getPassword(), "123");
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user.getLogin(), "Mark");
+        Assert.assertEquals(user.getPassword(), "123");
     }
 
     @Test
     public void testGetUserByLoginPassword() {
         User user = UserDAOImpl.getUserByLoginAndPassword("Mark", "123");
-        Assertions.assertNotNull(user);
-        Assertions.assertEquals(user.getLogin(), "Mark");
-        Assertions.assertEquals(user.getPassword(), "123");
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user.getLogin(), "Mark");
+        Assert.assertEquals(user.getPassword(), "123");
     }
 
 }
