f376e51cd63708a4c3736599b7b4f97bafe32ccf
Pavel Dorozhko <pavel.dorozhko@gmail.com>
2016-12-29 16:16:04 +0300

    add new sql and bug fixing

--- src/main/java/com/clinic/mapper/UserMapper.java
+++ src/main/java/com/clinic/mapper/UserMapper.java
@@ -6,10 +6,11 @@ import org.apache.ibatis.annotations.*;
 public interface UserMapper {
 
     final String getUserById = "SELECT * FROM user WHERE id=#{id}";
+    final String getUserByLogin = "SELECT * FROM user WHERE login=#{login}";
     final String getUserByLoginPassword = "SELECT * FROM user WHERE login=#{login} AND password=#{password}";
     final String getUserByRole = "SELECT * FROM user WHERE role=#{role}";
     final String addNewUser = "INSERT INTO user (login, password, role, name, phone, address) VALUES (#{login}, #{password}, #{role}, #{name}, #{phone}, #{address})";
-    final String updateUserInfoByLogin = "UPDATE user SET  #{password}, #{role}, #{name}, #{phone}, #{address} WHERE login=#{login}";
+    final String updateUserInfoByLogin = "UPDATE user SET password=#{password}, role=#{role}, name=#{name}, phone=#{phone}, address=#{address} WHERE login=#{login}";
     final String deleteUser = "DELETE FROM user WHERE login=#{login}";
 
     @Results(
@@ -19,13 +20,16 @@ public interface UserMapper {
     )
 
     @Select(getUserById)
-    User getUser(int id);
+    User getUserById(int id);
 
     @Select(getUserByRole)
     User getUserByRole(byte role);
 
     @Select(getUserByLoginPassword)
     User getUserByLoginAndPassword(@Param("login")String login, @Param("password")String password);
+
+    @Select(getUserByLogin)
+    User getUserByLogin(String login);
 
     @Insert(addNewUser)
     void addNewUser(@Param("login")String login, @Param("password")String password, @Param("role")byte role, @Param("name")String name, @Param("phone")String phone, @Param("address")String address);
