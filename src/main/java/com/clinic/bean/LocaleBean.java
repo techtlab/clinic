96431e4db711cf2e11f67b6e9a18eb3a0e37ad9c
Evgeny Yanul <evgeny.yanul@gmail.com>
2017-01-05 21:04:35 +0200

    Update UI. Languages support

---
+++ src/main/java/com/clinic/bean/LocaleBean.java
@@ -0,0 +1,47 @@
+package com.clinic.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@ManagedBean(name = "localeBean")
+@SessionScoped
+public class LocaleBean {
+
+    private String localeCode;
+
+    private static Map<String,Object> countries;
+    static{
+        countries = new LinkedHashMap<String,Object>();
+        countries.put("English", Locale.ENGLISH);
+    }
+
+
+    public void countryLocaleCodeChanged(ValueChangeEvent e){
+        String newLocaleValue = e.getNewValue().toString();
+
+        for (Map.Entry<String, Object> entry : countries.entrySet()) {
+            if(entry.getValue().toString().equals(newLocaleValue)){
+                FacesContext.getCurrentInstance()
+                        .getViewRoot().setLocale((Locale)entry.getValue());
+            }
+        }
+    }
+
+    public Map<String, Object> getCountriesInMap() {
+        return countries;
+    }
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+}
